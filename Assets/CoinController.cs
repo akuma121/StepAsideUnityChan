5091899c18c0a9bb9efde304eac1392941d04e8d
akuma121 <72017747+akuma121@users.noreply.github.com>
2020-12-16 20:17:50 +0900

    課題提出

---
+++ Assets/CoinController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.Rotate(0, Random.Range(0, 360), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, 3, 0);
+    }
+}
---
+++ Assets/DestroygameObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroygameObject : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "DestroyWellTag")
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
